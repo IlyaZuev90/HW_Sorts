be574d6b1a9139cb0f353e7f13a0797478be6252
Ilya_Zuev_90 <god_of_iwowi@mail.ru>
2023-07-21 16:30:46 +0400

    Reformated code

--- src/main/java/ru/netology/sorts/AviaSouls.java
+++ src/main/java/ru/netology/sorts/AviaSouls.java
@@ -9,22 +9,22 @@ public class AviaSouls {
 
     /**
      * Вспомогательный метод для имитации добавления элемента в массив
+     *
      * @param current Массив, в который мы хотим добавить элемент
-     * @param ticket Элемент, который мы хотим добавить
+     * @param ticket  Элемент, который мы хотим добавить
      * @return Возвращает новый массив, который выглядит как тот что мы передали,
      * но с добавлением нового элемента в конец
      */
     private Ticket[] addToArray(Ticket[] current, Ticket ticket) {
         Ticket[] tmp = new Ticket[current.length + 1];
-        for (int i = 0; i < current.length; i++) {
-            tmp[i] = current[i];
-        }
+        System.arraycopy(current, 0, tmp, 0, current.length);
         tmp[tmp.length - 1] = ticket;
         return tmp;
     }
 
     /**
      * Метод добавления билета в менеджер
+     *
      * @param ticket Добавляемый билет
      */
     public void add(Ticket ticket) {
@@ -37,8 +37,9 @@ public class AviaSouls {
 
     /**
      * Метод поиска билетов по маршруту
+     *
      * @param from Откуда вылетаем
-     * @param to Куда прилетаем
+     * @param to   Куда прилетаем
      * @return Массив из подходящих билетов
      */
     public Ticket[] search(String from, String to) {
--- src/main/java/ru/netology/sorts/Ticket.java
+++ src/main/java/ru/netology/sorts/Ticket.java
@@ -1,13 +1,14 @@
 package ru.netology.sorts;
 
 import java.util.Objects;
-public class Ticket implements Comparable<Ticket> {
-    private String from; // аэропорт откуда
-    private String to; // аэропорт куда
-    private int price; // цена
 
-    private int timeFrom; // время вылета (по москве)
-    private int timeTo; // время прилёта (по москве)
+public class Ticket implements Comparable<Ticket> {
+    private final String from; // аэропорт откуда
+    private final String to; // аэропорт куда
+    private final int price; // цена
+
+    private final int timeFrom; // время вылета (по москве)
+    private final int timeTo; // время прилёта (по москве)
 
     public Ticket(String from, String to, int price, int timeFrom, int timeTo) {
         this.from = from;
